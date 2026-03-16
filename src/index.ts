240417d6b00618b9b2239f37f81ba90aae4ff441
Robert Thornton <robert@pebble-tree.co.uk>
2026-03-16 13:28:21 +0000

    export types from ./tokenTypes

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export { TokenFlags } from "./tokenRingTypes"
+export * from "./tokenRingTypes"
 export type { Token, TokenRingRegistrationKey, TokenRingOptions, TokenRingConfig, TokenRingWorkDistributorInterface } from "./tokenRingTypes"
 export { TokenRingWorkDistributor } from "./tokenRing"
